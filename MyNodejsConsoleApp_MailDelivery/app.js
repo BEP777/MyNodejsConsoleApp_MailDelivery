358b03867dce107f5883abdbd379cfa3c3bf32c6
Avshalom <avshalom.tam@gmail.com>
2019-01-31 23:54:33 +0200

    installed 2 new modules

--- MyNodejsConsoleApp_MailDelivery/app.js
+++ MyNodejsConsoleApp_MailDelivery/app.js
@@ -1,3 +1,55 @@
-'use strict';
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var events = require('events');
+//var upper = require('node_modules/upper-case/upper-case.js');
+var uc = require('upper-case');
 
-console.log('Hello world');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+    console.log("i hear a scream!");
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+
+
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.write(uc("Hello World!"));
+//    res.end();
+//}).listen(8080);
+
+
+
+////create the server
+//http.createServer(function (req, res) {
+//    //build the path to the right file that will will be sent to the client
+//    //according to its request
+//    var q = url.parse(req.url, true);
+//    var filename = "." + q.pathname;
+//   //read from the right file
+//    fs.readFile(filename, function (err, data) {
+//        if (err) {
+//            res.writeHead(400, { 'Content-Type': 'text/html' }); 
+//            return res.end("404 Not Found");
+//        }
+//        res.writeHead(200, { 'Content-Type': 'text/html' });
+//        //send it to the client
+//        res.write(data);
+//        return res.end(); 
+//    });
+//}).listen(8080);
+
+//create a file named mynewfile1.txt:
+//fs.unlink('mynewfile1.txt', function (err) {
+//fs.appendFile('mynewfile1.txt', 'this is update', function (err) {
+//    if (err) throw err;
+//    console.log('Saved!');
+//});
+
